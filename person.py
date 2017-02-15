b654150b5410500b416dccf7325ece840697c954
Rishav Agarwal <Rishav Agarwal>
2017-02-15 18:00:23 +0530

    person commit

---
+++ person.py
@@ -0,0 +1,12 @@
+class person:
+    def __init__(self,name,age):
+        self.age=age
+        self.name=name
+    def Getname(self):
+        print(self.name)
+        print(self.age)
+
+
+
+
+    
